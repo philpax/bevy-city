9c81bb4d128a37e4f839c8c314b38ad62662d384
Philpax <me@philpax.me>
2022-05-14 08:30:59 +0200

    feat(rwf): add BinaryStreamFile::from_bytes

--- crates/renderware-format/src/lib.rs
+++ crates/renderware-format/src/lib.rs
@@ -528,10 +528,13 @@ impl BinaryStreamFile {
     pub fn from_reader<R: io::Read + io::Seek + Send + 'static>(
         mut reader: R,
     ) -> Result<Self, Error> {
-        // We only support one section for now
         let mut data = vec![];
         reader.read_to_end(&mut data)?;
+        Self::from_bytes(&data)
+    }
 
+    pub fn from_bytes(data: &[u8]) -> Result<Self, Error> {
+        // We only support one section for now
         let (_, section) = Section::parse(&data, None)
             .map_err(|err| err.map_input(|i| UnparsedData(i.to_owned())))
             .finish()?;
