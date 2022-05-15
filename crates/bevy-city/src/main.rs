b769b19d5706bd2240dc4f50b9afc48962e9cd70
Philpax <me@philpax.me>
2022-05-15 07:02:54 +0200

    feat(bc): wip ipl/map loading

--- crates/bevy-city/src/main.rs
+++ crates/bevy-city/src/main.rs
@@ -1,12 +1,18 @@
 use bevy::prelude::*;
+use bevy_editor_pls::prelude::*;
+use maps::ipl_parser::Ipl;
+
+pub mod maps;
 
 fn main() {
     App::new()
         .insert_resource(Msaa { samples: 4 })
         .add_plugins(DefaultPlugins)
         .add_plugin(bevy_renderware::RwPlugin)
+        .add_plugin(maps::ipl_parser::IplPlugin)
         .add_plugin(EditorPlugin)
         .add_startup_system(setup)
+        // .add_system(load_map)
         .run();
 }
 
@@ -15,10 +21,11 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let model_handle = asset_server.load("models/gta3/uzi.dff");
+    let downtown_handle: Handle<Ipl> = asset_server.load("data/maps/downtown/downtown.ipl");
+    commands.insert_resource(downtown_handle);
 
     commands.spawn_bundle(PbrBundle {
-        mesh: model_handle,
+        mesh: asset_server.load("models/gta3/uzi.dff"),
         material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
@@ -37,4 +44,45 @@ fn setup(
         transform: Transform::from_xyz(10.0, 10.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+fn load_map(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+    mut ev_asset: EventReader<AssetEvent<Ipl>>,
+    assets: Res<Assets<Ipl>>,
+) {
+    for ev in ev_asset.iter() {
+        match ev {
+            AssetEvent::Created { handle } => {
+                let ipl = assets.get(handle).unwrap();
+                let avg_position = ipl
+                    .instances
+                    .iter()
+                    .map(|(_, pos)| Vec3::new(pos[0], pos[2], pos[1]))
+                    .reduce(|acc, pos| acc + pos)
+                    .unwrap_or_default()
+                    / (ipl.instances.len() as f32);
+
+                for (name, position) in &ipl.instances {
+                    let path = format!("models/gta3/{name}.dff");
+                    let model_handle = asset_server.load(&path);
+
+                    commands.spawn_bundle(PbrBundle {
+                        mesh: model_handle,
+                        material: materials.add(Color::rgb(1.0, 1.0, 1.0).into()),
+                        transform: Transform::from_translation(
+                            Vec3::new(position[0], position[2], position[1]) - avg_position,
+                        ),
+                        ..default()
+                    });
+                }
+            }
+            AssetEvent::Modified { handle: _handle } => {
+                panic!("you aren't meant to modify the IPLs during gameplay!");
+            }
+            AssetEvent::Removed { handle: _handle } => {}
+        }
+    }
 }
---
+++ crates/bevy-city/src/maps/ipl_parser.rs
@@ -0,0 +1,110 @@
+use bevy::{
+    app::prelude::*,
+    asset::{AddAsset, AssetLoader, LoadedAsset},
+    reflect::TypeUuid,
+    utils::BoxedFuture,
+};
+use std::collections::HashMap;
+
+#[derive(Debug, TypeUuid)]
+#[uuid = "eef31d55-f995-4073-87a0-3c50e7fabef7"]
+pub struct Ipl {
+    pub instances: Vec<(String, [f32; 3])>,
+}
+
+impl Ipl {
+    pub fn parse(data: &str) -> Self {
+        let mut current_section = None;
+        let mut sections: HashMap<&str, Vec<&str>> = HashMap::new();
+        for line in data.lines() {
+            if line.starts_with("#") {
+                continue;
+            }
+
+            if let Some(section) = current_section {
+                if line == "end" {
+                    current_section = None;
+                } else {
+                    sections.get_mut(section).unwrap().push(line);
+                }
+            } else {
+                current_section = Some(line);
+                sections.insert(line, vec![]);
+            }
+        }
+
+        let instances: Vec<_> = sections
+            .get("inst")
+            .expect("no inst")
+            .iter()
+            .map(|line| {
+                let segments: Vec<_> = line.split(",").map(|s| s.trim()).collect();
+                (
+                    segments[1].to_string(),
+                    [
+                        segments[3].parse::<f32>().unwrap(),
+                        segments[4].parse::<f32>().unwrap(),
+                        segments[5].parse::<f32>().unwrap(),
+                    ],
+                )
+            })
+            .collect();
+
+        Ipl { instances }
+    }
+}
+
+#[derive(Default)]
+pub struct IplLoader;
+
+impl AssetLoader for IplLoader {
+    fn load<'a>(
+        &'a self,
+        bytes: &'a [u8],
+        load_context: &'a mut bevy::asset::LoadContext,
+    ) -> BoxedFuture<'a, anyhow::Result<()>> {
+        Box::pin(async move {
+            let value = Ipl::parse(std::str::from_utf8(bytes).unwrap());
+            load_context.set_default_asset(LoadedAsset::new(value));
+            Ok(())
+        })
+    }
+
+    fn extensions(&self) -> &[&str] {
+        static EXTENSIONS: &[&str] = &["ipl"];
+        EXTENSIONS
+    }
+}
+
+/// Adds support for Ipl file loading to Apps
+#[derive(Default)]
+pub struct IplPlugin;
+impl Plugin for IplPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_asset::<Ipl>().init_asset_loader::<IplLoader>();
+    }
+}
+
+mod tests {
+    #[test]
+    fn can_parse_downtown_subset() {
+        const TEST_DATA: &str = r"
+# IPL generated from Max file downtown.max
+inst
+1860, doontoon03, 0, -445.4862671, 1280.132813, 42.78390503, 1, 1, 1, 0, 0, 0, 1
+1861, doontoon04, 0, -303.8299866, 1394.506836, 6.610000134, 1, 1, 1, 0, 0, 0, 1
+1862, doontoon09, 0, -798.4454346, 1039.305176, 12.29159546, 1, 1, 1, 0, 0, 0, 1
+end
+cull
+end
+pick
+end
+path
+end
+";
+
+        let test_data = TEST_DATA.trim();
+        let ipl = super::Ipl::parse(test_data);
+        println!("{:?}", ipl);
+    }
+}
