e7690e2e8ffbb933e61a2a9dab4b1dc04447ec82
Philpax <me@philpax.me>
2022-05-18 03:35:57 +0200

    fix(bc): make categorise_lines ignore empty lines

--- crates/bevy-city/src/assets/common.rs
+++ crates/bevy-city/src/assets/common.rs
@@ -4,7 +4,7 @@ pub fn categorise_lines(data: &str) -> HashMap<&str, Vec<&str>> {
     let mut current_section = None;
     let mut sections: HashMap<&str, Vec<&str>> = HashMap::new();
     for line in data.lines() {
-        if line.starts_with('#') {
+        if line.starts_with('#') || line.trim().is_empty() {
             continue;
         }
 
@@ -30,6 +30,32 @@ pub fn split_line(line: &str) -> Vec<&str> {
 
 mod tests {
     pub use super::*;
+
+    #[test]
+    fn can_categorise_some_lines() {
+        const TEST_DATA: &str = r#"
+peds
+9, HFYST, HFYST, CIVFEMALE, STAT_STREET_GIRL, sexywoman, 013, null, 6,1		
+83, CBa, CBa, GANG1, STAT_GANG1, gang1, 0, null, 6,6
+end
+
+cars
+130,	landstal, 	landstal, 	car, 	LANDSTAL, 	LANDSTK, 		null,	normal, 	10,	7,	0,		254, 0.8
+end
+
+"#;
+
+        let test_data = TEST_DATA.trim();
+        assert_eq!(categorise_lines(test_data), HashMap::from([
+            ("peds", vec![
+                "9, HFYST, HFYST, CIVFEMALE, STAT_STREET_GIRL, sexywoman, 013, null, 6,1\t\t",
+                "83, CBa, CBa, GANG1, STAT_GANG1, gang1, 0, null, 6,6",
+            ]),
+            ("cars", vec![
+                "130,\tlandstal, \tlandstal, \tcar, \tLANDSTAL, \tLANDSTK, \t\tnull,\tnormal, \t10,\t7,\t0,\t\t254, 0.8",
+            ])
+        ]));
+    }
 
     #[test]
     fn can_split_line_with_mix_of_commas_and_spaces() {
